4b47dd1c2d564ca9f3e399d5e7ba9b2915f60d77
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2018-10-06 12:57:38 -0300

    improv guitar.html

--- run.py
+++ run.py
@@ -8,7 +8,10 @@ from scalaGenerator import get_scale, get_all_scales
 guitarContext = {
     "strings": get_guitar(),
     "notes": common.notes,
-    "scale": get_scale('major', common.get_note('C'))
+    "allScales": {
+        "major": [get_scale('major', common.get_note(note)) for note in common.notes],
+        "minor": [get_scale('minor', common.get_note(note)) for note in common.notes]
+    }
 }
 
 scaleContext = {
