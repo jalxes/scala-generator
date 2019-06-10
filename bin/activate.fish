33ad9052340204ce8bd77ba7fd31e328e8671d13
jalxes <devjeffersonsilveira@gmail.com>
2019-06-10 00:00:01 -0300

    fixed namespace

--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jalxes/Projects/escala-generator"
+set -gx VIRTUAL_ENV "/home/jalxes/Projects/scala-generator"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
